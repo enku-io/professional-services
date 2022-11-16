543d0931cf1ff0295bd25800419bba13b74007d1
arod0719 <alexanderrod@google.com>
2022-11-16 21:53:36 +0000

    metadata module

--- 05-vertexai/metadata_store.tf
+++ 05-vertexai/metadata_store.tf
@@ -1,9 +1,3 @@
-# Doc: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/vertex_ai_metadata_store
-
-# Basic Metadata Store
-resource "google_vertex_ai_metadata_store" "store" {
-  name          = "default"
-  description   = "Store to test the terraform module"
-  region        = "us-central1"
-  provider      = google-beta
+module "vertex-metadata" {
+  source     = "../modules/vertex-metadata"
 }
