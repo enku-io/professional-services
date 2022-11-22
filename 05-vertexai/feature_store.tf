2e5d6a03785275481ba4f973b5d6ef772dd8d42a
Enku Feleke <enkuwfeleke@google.com>
2022-11-22 10:31:41 -0600

    Add feature-store module

--- 05-vertexai/feature_store.tf
+++ 05-vertexai/feature_store.tf
@@ -1,45 +1,122 @@
-# Docs: 
-# Feature store: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/vertex_ai_featurestore
-# Entity Type: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/vertex_ai_featurestore_entitytype
-# Feature: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/vertex_ai_featurestore_entitytype_feature
+# # Docs: 
+# # Feature store: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/vertex_ai_featurestore
+# # Entity Type: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/vertex_ai_featurestore_entitytype
+# # Feature: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/vertex_ai_featurestore_entitytype_feature
 
-# Basic online serving Featurestore
-resource "google_vertex_ai_featurestore" "featurestore" {
-  provider = google-beta
-  name     = "terraform_fs_example"
-  labels = {
-    foo = "bar"
-  }
-  region   = "us-central1"
-  online_serving_config {
-    fixed_node_count = 2 #Enables online serving 
-  }
-}
-
-# EntityType monitoring the Featurestore
-resource "google_vertex_ai_featurestore_entitytype" "entity" {
-  provider = google-beta
-  name     = "terraform_et_example"
-  labels = {
-    foo = "bar"
-  }
-  featurestore = google_vertex_ai_featurestore.featurestore.id
-  monitoring_config {
-    snapshot_analysis {
-      disabled = false
-      monitoring_interval = "86400s"
+module "vertexai-featurestore" {
+  source = "../modules/vertexai-featurestore"
+  project = "vertex-ai-wf-9fa5"
+  providers = {
+    google-beta = google-beta
+   }
+  featurestore = {
+    "test_featurestore" = {
+        "name" = "movie_featurestore"
+        "labels"  = {
+            "label1" = "value1"
+        }
+        "region" = "us-central1"
+        "online_serving_config" = {
+            "fixed_node_count" = 1 
+        }
+        "entities" = [
+          {
+            "name" = "name"
+            "labels" = {
+              "label1" = "value1"
+            }
+            "monitoring_config" = {
+              "snapshot_analysis" = {
+                "disabled" = false
+                "monitoring_interval" = "86400s"
+              }
+            }
+            "features" = [
+              {
+                "name" = "name"
+                "labels" = {
+                  "label1" = "value1"
+                }
+                "value_type"  = "INT64_ARRAY"
+              }
+            ]
+          },
+          {
+            "name" = "rating"
+            "labels" = {
+              "label1" = "value1"
+            }
+            "monitoring_config" = {
+              "snapshot_analysis" = {
+                "disabled" = false
+                "monitoring_interval" = "86400s"
+              }
+            }
+            "features" = [
+              {
+                "name" = "name"
+                "labels" = {
+                  "label1" = "value1"
+                }
+                "value_type"  = "INT64_ARRAY"
+              }
+            ]
+          },
+          {
+            "name" = "created_time"
+            "labels" = {
+              "label1" = "value1"
+            }
+            "monitoring_config" = {
+              "snapshot_analysis" = {
+                "disabled" = false
+                "monitoring_interval" = "86400s"
+              }
+            }
+            "features" = [
+              {
+                "name" = "name"
+                "labels" = {
+                  "label1" = "value1"
+                }
+                "value_type"  = "INT64_ARRAY"
+              }
+            ]
+          }
+        ]
+    }
+    "test_featurestore_2" = {
+        "name" = "car_featurestore"
+        "labels"  = {
+            "label1" = "value1"
+        }
+        "region" = "us-central1"
+        "online_serving_config" = {
+            "fixed_node_count" = 1 
+        }
+        "entities" = [
+          {
+            "name" = "name"
+            "labels" = {
+              "label1" = "value1"
+            }
+            "monitoring_config" = {
+              "snapshot_analysis" = {
+                "disabled" = false
+                "monitoring_interval" = "86400s"
+              }
+            }
+            "features" = [
+              {
+                "name" = "name"
+                "labels" = {
+                  "label1" = "value1"
+                }
+                "value_type"  = "INT64_ARRAY"
+              }
+            ]
+          }
+        ]
     }
   }
-}
-
-# Feature Integer sitting inside the EntityType
-resource "google_vertex_ai_featurestore_entitytype_feature" "feature" {
-  provider = google-beta
-  name     = "terraform_f_example"
-  labels = {
-    foo = "bar"
-  }
-  entitytype = google_vertex_ai_featurestore_entitytype.entity.id
-
-  value_type = "INT64_ARRAY"
 }
